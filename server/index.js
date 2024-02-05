278663706d903886d484a7c22d58438798c95f7f
Jeet Singh <jeetmia1@gmail.com>
2024-02-05 21:30:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose');
 const cors = require('cors');
 const corsOptions ={
-    origin:'https://suki-rho.vercel.app', 
+    origin:'*', 
     methods:["POST", "GET"],
     credentials:true,            //access-control-allow-credentials:true
     
